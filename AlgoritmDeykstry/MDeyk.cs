1414f4aa169ae49f5697e4f944d4fdb2e851b20f
Goncharova <Goncharova@gmail.com>
2024-04-26 14:59:36 +0300

    need thinking

---
+++ AlgoritmDeykstry/MDeyk.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmDeykstry
+{
+    internal class MDeyk
+    {
+        public void Deyk(int[,] distance, int[] Pdestin, int[] Pdepar)
+        {
+            //Console.Write("Введите начальная точка: ");
+            int start = 1;/*int.Parse(Console.ReadLine());*/
+            int[] ves = new int[Pdepar.Length];
+
+            for (int i = 0; i < ves.Length;)
+            {
+                for (int disI = 0; disI < distance.GetLength(0); disI++)
+                {                    
+                    for (int disJ = 0; disJ < distance.GetLength(1);)
+                    {   
+                        if (distance[disI, disJ] != 0) 
+                        {
+                            if (i == start)
+                            {
+                                i++;
+                                ves[Pdestin[i]] = distance[disI, disJ];
+                                continue;
+                            }
+                            ves[Pdestin[i]] = distance[disI, disJ]; i++; disJ++;
+                        }  
+                    }
+                }
+            }
+            for (int j = 0; j < ves.GetLength(1); j++)
+            {
+                Console.Write(ves[j] + "\t");
+            }
+        }
+    }
+}
--- AlgoritmDeykstry/Program.cs
+++ AlgoritmDeykstry/Program.cs
@@ -4,7 +4,47 @@
     {
         static void Main(string[] args)
         {
-            
+            MDeyk mD = new MDeyk();
+            //Console.Write("Введите количество пунктов назначения: ");
+            int destin = 6/*int.Parse(Console.ReadLine())*/;
+            //Console.Write("Введите количество пунктов отправления: ");
+            int depar = 6/*int.Parse(Console.ReadLine())*/;            
+            int[,] distance = new int[destin, depar];
+            int[] Pdestin = new int[destin];
+            int[] Pdepar = new int[depar];
+            for (int i = 0; i < Pdestin.Length; i++)
+            {
+                Pdestin[i] = i;
+            }
+            for (int j = 0; j < Pdepar.Length; j++)
+            {
+                Pdepar[j] = j;
+            }
+            using (StreamReader read = new StreamReader("Deyk.csv"))
+            {
+                while (!read.EndOfStream)
+                {
+                    for (int i = 0; i < destin; i++)
+                    {
+                        string line = read.ReadLine();
+                        string[] str = line.Split(';');
+                        for (int j = 0; j < depar; j++)
+                        {
+                            distance[i, j] = int.Parse(str[j]);
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("\nМатрица с исходными данными\n");            
+            for (int i = 0; i < distance.GetLength(0); i++)
+            {                
+                for (int j = 0; j < distance.GetLength(1); j++)
+                {                    
+                    Console.Write(distance[i, j] + "\t");                    
+                }
+                Console.WriteLine();
+            }
+            mD.Deyk(distance, Pdestin, Pdepar);
         }
     }
 }
